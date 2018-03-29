20f0d46581ac4d1fcda6c5a2aabe89af1950cf17
youlookwhat <770413277@qq.com>
2018-03-29 20:14:38 +0800

    add Widget

---
+++ app/src/main/java/com/example/jingbin/materialdesign/widget/NewAppWidget.java
@@ -0,0 +1,62 @@
+package com.example.jingbin.materialdesign.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.jingbin.materialdesign.MainActivity;
+import com.example.jingbin.materialdesign.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+        // 打开MainActivity
+        views.setOnClickPendingIntent(R.id.appwidget_text, getOpenPendingIntent(context));
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    /**
+     * 获取 打开 MainActivity 的 PendingIntent
+     */
+    private static PendingIntent getOpenPendingIntent(Context context) {
+        Intent intent = new Intent();
+        intent.setClass(context, MainActivity.class);
+//        intent.putExtra("main", "这句话是我从桌面点开传过去的。");
+        PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+        return pi;
+    }
+
+}
+
